add787666cd71883d779ee796ad354a1fd953bf6
Shegzyrey <54408445+Shegzyrey@users.noreply.github.com>
2021-04-15 18:03:57 -0700

    Update logout1.php

--- logout1.php
+++ logout1.php
@@ -2,7 +2,7 @@
 session_start();
 if(isset($_SESSION['logged'])){
 echo 'You are still logged in ';
-echo '<br><a href="destroy.php?logout=true">Logout</a>';
+echo '<br><a href="logout.php?logout=true">Logout</a>';
 } else {
 echo 'Please login first';
 }
