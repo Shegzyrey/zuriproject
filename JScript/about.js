234edcdb20bcf583c32886a5d69b25ae09af46c1
Shegzyrey <54408445+Shegzyrey@users.noreply.github.com>
2022-06-23 22:30:08 -0400

    Add files via upload

---
+++ JScript/about.js
@@ -0,0 +1,4 @@
+//write a javascripts code that prints out name, height and country on the screen.
+console.log('Name: Dada Oluwasegun');
+console.log('Height: 1.76cm');
+console.log('Country: Nigeria');
