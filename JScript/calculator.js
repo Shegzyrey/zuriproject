008ecb8f9647702b449cc2b83dbad32f8c7068a4
Shegzyrey <54408445+Shegzyrey@users.noreply.github.com>
2022-06-24 17:45:22 -0400

    Add files via upload

---
+++ JScript/calculator.js
@@ -0,0 +1,22 @@
+var num1;
+var num2;
+
+function addition(num1, num2)
+{
+    return (num1 + num2);
+}
+
+function subtraction(num1, num2)
+{
+    return (num1 - num2);
+}
+
+function multiplication(num1, num2)
+{
+    return (num1 * num2);
+}
+
+function division(num1, num2)
+{
+    return (num1 / num2);
+}
