f183b9cf242fe8f91e36fe0ef32425fbb73716ec
Shegzyrey <54408445+Shegzyrey@users.noreply.github.com>
2022-06-24 14:44:54 -0700

    Delete JScript directory

--- JScript/about.js
+++
@@ -1,4 +0,0 @@
-//write a javascripts code that prints out name, height and country on the screen.
-console.log('Name: Dada Oluwasegun');
-console.log('Height: 1.76cm');
-console.log('Country: Nigeria');
